f95105cb1e44461e80632283b84dc87e8fc1a797
mon <sawmon71293@gmail.com>
2025-07-10 18:41:23 +0700

    configure deployment terraform

--- infra/deploy/variables.tf
+++ infra/deploy/variables.tf
@@ -1,14 +1,14 @@
- variable "prefix" {
-   description = "Prefix for resources in AWS"
-   default="raa"
- }
+variable "prefix" {
+  description = "Prefix for resources in AWS"
+  default     = "raa"
+}
 
 variable "project" {
   description = "Project name for tagging resources"
-  default = "recipe app api"
+  default     = "recipe app api"
 }
 
 variable "contact" {
-    description = "contact email for tagging resources"
-    default = "sawmon71293@gmail.com"
+  description = "contact email for tagging resources"
+  default     = "sawmon71293@gmail.com"
 }
